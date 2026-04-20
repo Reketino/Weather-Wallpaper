f98ba98676d70e524b2e688e7ade22d18bd6ce0c
Reketino <bjornevensk8@gmail.com>
2026-04-20 11:00:25 +0200

    feat: new feature

--- WeatherWallpaper.Infrastructure/OpenWeatherService.cs
+++ WeatherWallpaper.Infrastructure/OpenWeatherService.cs
@@ -24,6 +24,7 @@ public class OpenWeatherService : IWeatherService
         var instant = timeseries
             .GetProperty("data")
             .GetProperty("instant")
+            .GetProperty("details");
     }
 
 }
