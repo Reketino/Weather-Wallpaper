1ed9d4c27edcff24d6be3533897f30514b07e372
Reketino <bjornevensk8@gmail.com>
2026-04-20 10:53:07 +0200

    feat: setup var data to use json document parsing

--- WeatherWallpaper.Infrastructure/OpenWeatherService.cs
+++ WeatherWallpaper.Infrastructure/OpenWeatherService.cs
@@ -14,6 +14,8 @@ public class OpenWeatherService : IWeatherService
 
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         request.Headers.Add("User-Agent", "weather-wallpaper-app"); 
+
+        var data = JsonDocument.Parse(json); 
     }
 
 }
