77d292f27c4de68a0607c3b071ca53c7ffb819a8
Reketino <bjornevensk8@gmail.com>
2026-04-23 10:58:38 +0200

    chore: removed UnmanagedType.Bool

--- WeatherWallpaper.Infrastructure/WindowsWallpaperService.cs
+++ WeatherWallpaper.Infrastructure/WindowsWallpaperService.cs
@@ -10,6 +10,7 @@ public partial class WindowsWallpaperService : IWallpaperService
     private const int SPIF_SENDCHANGE = 0X02;
 
     [LibraryImport("user32.dll", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
+    
     private static partial bool SystemParametersInfo(
         int uAction,
         int uParam,
