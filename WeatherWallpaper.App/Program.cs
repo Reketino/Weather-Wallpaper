19e89a122621846e07664a617876f85779e95eee
Reketino <bjornevensk8@gmail.com>
2026-04-24 10:05:06 +0200

    feat: setup & defined IWallpaper service

--- WeatherWallpaper.App/Program.cs
+++ WeatherWallpaper.App/Program.cs
@@ -4,3 +4,4 @@ using WeatherWallpaper.Infrastructure;
 Console.WriteLine("Weather Wallpaper starting...");
 
 IWeatherService weatherService = new MetWeatherService();
+IWallpaperService wallpaperService = new WindowsWallpaperService();
