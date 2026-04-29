193414bf769a38875ce46411ea3b8ec6a4f3ab16
Reketino <bjornevensk8@gmail.com>
2026-04-29 11:11:24 +0200

    feat: setup namespace

--- WeatherWallpaper.Tray/Program.cs
+++ WeatherWallpaper.Tray/Program.cs
@@ -3,3 +3,5 @@ using System.Windows.Forms;
 using WeatherWallpaper.Core;
 using WeatherWallpaper.Infrastructure;
 using WeatherWallpaper.Infrastructure.Wallpapers;
+
+namespace WeatherWallpaper.Tray;
