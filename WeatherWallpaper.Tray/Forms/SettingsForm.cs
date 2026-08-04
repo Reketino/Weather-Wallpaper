c1342e6bea877b7b05e1d0fe97054464eea3d176
Reketino <bjornevensk8@gmail.com>
2026-08-05 00:01:22 +0200

    feat: implement SettingsForm class with constructor

--- WeatherWallpaper.Tray/Forms/SettingsForm.cs
+++ WeatherWallpaper.Tray/Forms/SettingsForm.cs
@@ -2,3 +2,11 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace WeatherWallpaper.Tray.Forms;
+
+public sealed class SettingsForm : Form
+{
+    public SettingsForm()
+    {
+        
+    }
+}
